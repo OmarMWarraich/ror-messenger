9ce97238387075395950108e61edce7ece697e96
OmarMWarraich <earthling986@gmail.com>
2024-03-27 20:01:59 +0500

    feat: Display Msg from UI

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,6 +1,7 @@
 class ChatroomController < ApplicationController
   def index
     if user_signed_in?
+      @message = Message.new
       @messages = Message.all
     else
       redirect_to new_user_session_path
