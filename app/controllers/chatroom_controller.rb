d9a4d3487c8b730a99690354e06967d8447008cd
OmarMWarraich <earthling986@gmail.com>
2024-03-27 17:36:19 +0500

    feat: Devise Authentication, Views, User

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,4 +1,10 @@
 class ChatroomController < ApplicationController
   def index
+    if user_signed_in?
+      # display index.html.erb
+      
+    else
+      redirect_to new_user_session_path
+    end
   end
 end
